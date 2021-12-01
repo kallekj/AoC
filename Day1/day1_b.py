72d1ede9890239df74159960e674d703dfbc73f9
kallekj <kalle@djervbrant.net>
2021-12-01 13:04:47 +0100

    Day 1 done

--- Day1/day1_b.py
+++ Day1/day1_b.py
@@ -2,15 +2,23 @@
 with open('./input') as f:
     content = f.readlines()
 
-noNewLines = [line.lstrip('\n') for line in content]
+noNewLines = [int(line.lstrip('\n')) for line in content]
 
 prev = 0
+indexLow = 0
+indexHigh = 3
 count = 0
-for line in noNewLines:
+for i in range(len(noNewLines)):
+    if(indexHigh > len(noNewLines) + 1):
+        break
     if prev == 0:
-        prev = prev = int(line)
+        prev = sum(noNewLines[indexLow:indexHigh])
+        indexLow += 1
+        indexHigh += 1
         continue
-    if prev < int(line):
+    if prev < sum(noNewLines[indexLow:indexHigh]):
         count += 1
-    prev = int(line)
+    prev = sum(noNewLines[indexLow:indexHigh])
+    indexLow += 1
+    indexHigh += 1
 print(count)
