6545ae85b0e796b0d94722316fa398c3fdbb353b
kallekj <kalle@djervbrant.net>
2022-12-01 21:37:17 +0100

    Day-1 done

--- day-1/day-1a.py
+++ day-1/day-1a.py
@@ -1,6 +1,19 @@
+from functools import reduce
 import numpy as np
 
 with open('./input') as f:
-    content = f.readlines()
+    data = "".join(f.readlines())
 
-noNewLines = np.array([line.lstrip('\n') for line in content])
+
+def adder(a, b):
+    try:
+        return int(a)+int(b)
+    except Exception as e:
+        print(e)
+        return 0
+
+
+allElvs = list(map(lambda elf: reduce(adder, list(map(lambda food: int(food), filter(lambda cal: cal != '', elf.split('\n'))))), data.split('\n\n')))
+
+print(np.sum(sorted(allElvs)[-3:]))
+
