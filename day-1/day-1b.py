954d5dbca1d6f96a16ef0ebc06d51e76454632ce
kallekj <kalle@djervbrant.net>
2022-12-01 21:43:13 +0100

    Day-1 done

---
+++ day-1/day-1b.py
@@ -0,0 +1,14 @@
+from functools import reduce
+
+with open('./input') as f:
+    data = "".join(f.readlines())
+
+
+def adder(a, b):
+    return a + b
+
+
+allElvs = list(map(lambda elf: reduce(adder, list(map(lambda food: int(food), filter(lambda cal: cal != '', elf.split('\n'))))), data.split('\n\n')))
+
+print(max(allElvs))
+
