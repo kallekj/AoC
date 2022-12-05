c776081f4a4946348d663e6a4ddc17e8bc62b2e6
Karl-Johan Djervbrant <karl-johan-djervbrant@sogeti.se>
2022-12-05 11:49:15 +0100

    Day-5 done

---
+++ day-5/day-5a.py
@@ -0,0 +1,36 @@
+import re
+
+with open('./input') as f:
+    content = [line.rstrip('\n') for line in f.readlines()]
+
+creates = {}
+numCreates = 9
+for i in range(numCreates):
+    creates[i] = []
+
+def addCreate(char, pos):
+    if(char != ' '):
+        creates[pos].insert(0, char)
+
+
+def moveCreate(src, dst, num):
+    for _ in range(int(num)):
+        creates[int(dst)-1].append(creates[int(src)-1].pop())
+
+def printTopCases():
+    output = ""
+    for case in creates:
+        output += creates[case][-1]
+    print(output)
+
+for line in content:
+    if len(line) > 0 and line[1] not in ['1', 'o']:
+        for i in range(0, numCreates):
+            addCreate(line[i*4+1], i)
+    elif len(line) > 0 and line[1] != '1':
+        moves = re.findall(r'[0-9]{2}|[0-9]{1}', line)
+        moveCreate(moves[1], moves[2], moves[0])
+    else:
+        continue
+
+printTopCases()
