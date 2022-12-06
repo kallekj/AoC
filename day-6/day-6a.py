24d262c751c3a6c7802152a11dd2cb6c0bab8e01
Karl-Johan Djervbrant <karl-johan-djervbrant@sogeti.se>
2022-12-06 09:16:42 +0100

    Day-6 done

---
+++ day-6/day-6a.py
@@ -0,0 +1,15 @@
+windowSize = 4
+
+with open('./input') as f:
+    datastream = [ list(line.rstrip('\n')) for line in f.readlines()][0]
+
+def checkStartCode(sublist):
+    if len(set(sublist)) == 4:
+        return True
+    else:
+        return False
+
+for i in range(len(datastream) - windowSize + 1):
+    if checkStartCode(datastream[i:i+windowSize]):
+        print(i+windowSize)
+        break
