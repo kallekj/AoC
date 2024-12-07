90e6ad3da9fc79f0f954139f1b63c3a96f37474c
kallekj <kalle@djervbrant.net>
2024-12-07 16:42:30 +0100

    Complete day 4

--- day-4/b2.ts
+++ day-4/b2.ts
@@ -1,15 +1,4 @@
 import { transpose } from '../utils/array.ts';
-import { sum } from '../utils/math.ts';
-
-function isMAS(line: string) {
-    const MAS = /MAS/g;
-    return line.match(MAS);
-}
-
-function isSAM(line: string) {
-    const SAM = /SAM/g;
-    return line.match(SAM);
-}
 
 function isInside<T>(matrix: T[][], row: number, col: number): boolean {
     const maxRow = matrix.length - 1;
@@ -39,7 +28,7 @@ function isLetterX<T>(matrix: T[][], i: number, j: number, char: string): boolea
 }
 
 async function main() {
-    const input = 'test.txt';
+    const input = 'input.txt';
     const file = await Deno.readTextFile(input);
     const data = file.split('\n');
 
@@ -56,77 +45,19 @@ async function main() {
             const center = getLetter(matrix, i, j) ?? '';
             const isCenterLetterA = center === 'A';
             if (isCenterLetterA) {
-                const [TL, DR] = [
-                    getLetter(matrix, i - 1, j - 1) ?? '',
-                    getLetter(matrix, i + 1, j + 1) ?? '',
-                ];
-                const [TR, DL] = [
-                    getLetter(matrix, i - 1, j - 1) ?? '',
-                    getLetter(matrix, i + 1, j + 1) ?? '',
-                ];
-
-                const TLDR = ''.concat(...[TL, center, DR]);
-                const DRTL = ''.concat(...[DR, center, TL]);
-                const TRDL = ''.concat(...[TR, center, DL]);
-                const DLTR = ''.concat(...[DL, center, TR]);
-
-                // Check Top Left
-                if(isLetterX(matrix, i-1, j-1, 'S')){
-                    if(isSAM(TLDR)){
-                        // Check Top Right
-                        const isSamOrMas = isSAM(TRDL) || isMAS(TRDL);
-                        if(isSamOrMas){
-                            counter += 1;
-                        }
+                
+                if(isLetterX(matrix, i-1, j-1, 'M') && isLetterX(matrix, i+1, j+1, 'S')){
+                    if((isLetterX(matrix, i-1, j+1, 'M') && isLetterX(matrix, i+1, j-1, 'S')) || (isLetterX(matrix, i-1, j+1, 'S') && isLetterX(matrix, i+1, j-1, 'M'))){
+                        counter += 1;
                     }
                 }
 
-                if(isLetterX(matrix, i-1, j-1, 'M')){
-                    if(isMAS(TLDR)){
-                        // Check Top Right
-                        const isSamOrMas = isSAM(TRDL) || isMAS(TRDL);
-                        if(isSamOrMas){
-                            counter += 1;
-                        }
+                if(isLetterX(matrix, i-1, j-1, 'S') && isLetterX(matrix, i+1, j+1, 'M')){
+                    if((isLetterX(matrix, i-1, j+1, 'M') && isLetterX(matrix, i+1, j-1, 'S')) || (isLetterX(matrix, i-1, j+1, 'S') && isLetterX(matrix, i+1, j-1, 'M'))){
+                        counter += 1;
                     }
                 }
 
-                // if (isSAM(TLDR)) {
-                //     counter =
-                //         isSAM(TRDL) || isMAS(TRDL) ? (counter += 1) : counter;
-                //     continue;
-                // }
-
-                // if (isMAS(TLDR)) {
-                //     counter =
-                //         isSAM(TRDL) || isMAS(TRDL) ? (counter += 1) : counter;
-                //     continue;
-                // }
-
-
-                // if(isSAM(DRTL)){
-                //     xCounter = isSAM(DRTL) || isSAM(T) ? xCounter += 1 : xCounter;
-                // }
-
-                // Check SAM
-                // if TLDR + TRDL == SAM
-
-                // if DRTL + DRTL == SAM
-
-                // if TLDR + DRTL == SAM
-
-                // if DRTL + TRDL == SAM
-
-                // Check MAS
-                // if TLDR + TRDL == MAS
-
-                // if DRTL + DRTL == MAS
-
-                // if TLDR + DRTL == MAS
-
-                // if DRTL + TRDL == MAS
-
-                // Check SAM + MAS
             }
         }
     }
